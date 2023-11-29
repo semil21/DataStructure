4efa5f05c859307165f96ad10ab8ffcd4396a4e1
semil21 <m7semil@gmail.com>
2023-11-29 23:52:04 +0530

    new code

--- array/1-array-reverse.js
+++ array/1-array-reverse.js
@@ -1,5 +1,10 @@
-const arr = [1,2,3]
-
-for( i= arr.length ; i>=0 ; i--){
-    console.log(arr[i])
+const arr = [1, 4, 3, 2];
+const b = [];
+for (i = arr.length - 1; i > -1; i--) {
+  b.push(arr[i]);
 }
+const c = b.toString();
+let d = c.replaceAll(",", " ");
+console.log(d);
+
+//expected output = 2 3 4 1 

d5aaeb94718ae223709226bfa23c3584eff0fa67
semil21 <m7semil@gmail.com>
2023-12-05 00:04:10 +0530

    two sum

---
+++ array/2-two-sum.js
@@ -0,0 +1,13 @@
+// Input: nums = [2,7,11,15], target = 9
+
+let number = [1,2,3,4,5]
+let answer = 8
+
+for (let i=0 ; i <= number.length; i++){
+    for (let j =1; j<=number.length ; j++){
+        let total = i + j
+        if(total === answer){
+            console.log(i , j )
+        }
+    }
+}
